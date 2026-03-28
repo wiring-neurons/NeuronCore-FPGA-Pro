96155190074013bf9e31f9749e517088dde62f31
wiring-neurons <wiringneurons@gmail.com>
2026-03-28 21:09:07 +0530

    Create clk.v

---
+++ ALLMOD_UART/clk.v
@@ -0,0 +1,56 @@
+module clk(
+	input wire clk,
+	input wire [18:0] clk1_freq,
+	input wire [11:0] clk2_freq,
+	input wire [11:0] clk3_freq,
+	input wire [6:0]  clk4_freq,
+	output reg clk1,
+	output reg clk2,
+	output reg clk3,
+	output reg clk4
+);
+
+reg [4:0] clk1_counter = 0; // 25
+reg [17:0] clk2_counter = 0; // 1,56,250
+reg [17:0] clk3_counter = 0; // 1,56,250
+reg [19:0] clk4_counter = 0; // 6,25,000
+
+	always @(posedge clk) begin
+
+		clk1_counter <= clk1_counter + 1;
+		clk2_counter <= clk2_counter + 1;
+		clk3_counter <= clk3_counter + 1;
+		clk4_counter <= clk4_counter + 1;
+		
+		if(clk1_counter == 25000000/(clk1_freq*2)) begin
+		
+			clk1 <= ~clk1;
+			clk1_counter <= 0;
+		
+		end
+		
+		if(clk2_counter == 25000000/(clk2_freq *2)) begin
+		
+			clk2 <= ~clk2;
+			clk2_counter <= 0;
+		
+		end
+		
+		if(clk3_counter == 25000000/(clk3_freq *2)) begin
+		
+			clk3 <= ~clk3;
+			clk3_counter <= 0;
+		
+		end
+		
+		if(clk4_counter == 25000000/(clk4_freq *2)) begin
+		
+			clk4 <= ~clk4;
+			clk4_counter <= 0;
+		
+		end
+		
+
+	end
+
+endmodule
